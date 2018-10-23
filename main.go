68f2e9363f931bc6705bf63ee716ddf0e55704f2
Olexandr Reuta <oreuttc@training.local>
2018-10-23 18:12:59 +0300

    Added BeeGo structure

--- main.go
+++ main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	_ "github.com/oreuta/easytrip/routers"
+	"github.com/astaxie/beego"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "80"
-	}
-
-	http.HandleFunc("/hello", Hello)
-
-	log.Println("Server started...")
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatalf("ERROR: Failed to start Server (err: %v)", err)
-	}
+	beego.Run()
 }
 
-// Hello returns text message
-func Hello(w http.ResponseWriter, _ *http.Request) {
-	text := "<html><head></head><body>Hi from <b>Go</b> on Heroku!!!</body><html>"
-
-	count, err := w.Write([]byte(text))
-	if err != nil {
-		log.Printf("ERROR: %v", err)
-		return
-	}
-	log.Printf("SUCCESS: %d byte sent", count)
-}
