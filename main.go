ff177b6263abb63ca5c48de597e43d29b2dae285
Olexandr Reuta <oreuttc@training.local>
2018-10-23 18:30:26 +0300

    Added port processing

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	_ "github.com/oreuta/easytrip/routers"
+	"os"
+
 	"github.com/astaxie/beego"
+	_ "github.com/oreuta/easytrip/routers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := beego.AppConfig.Set("httpport", port)
+	if err != nil {
+		panic(err)
+	}
+
 	beego.Run()
 }
-
