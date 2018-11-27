56173e6fc7fab13618241a74d8fdffd58af8ea23
Dimitriy14 <yankovskiydy98@gmail.com>
2018-11-27 23:13:11 +0200

    Fixed statistic

--- controllers/statistics/statistics.go
+++ controllers/statistics/statistics.go
@@ -1,8 +1,6 @@
 package statistics
 
 import (
-	"time"
-
 	_ "github.com/oreuta/easytrip/controllers"
 	_ "github.com/oreuta/easytrip/controllers/bank-rating"
 	_ "github.com/oreuta/easytrip/controllers/best-bank"
@@ -16,10 +14,6 @@ type StatisticController struct {
 }
 
 func (this *StatisticController) Get() {
-	toolbox.StatisticsMap.AddStatistics("GET", "/", "&controllers.MainController", time.Duration(13000))
-	toolbox.StatisticsMap.AddStatistics("GET", "/comparision", "&controllers.bankRatingController.RatesController", time.Duration(14000))
-	toolbox.StatisticsMap.AddStatistics("GET", "/best", "&controllers.bestBankController.bestBankController", time.Duration(15000))
-
 	statistic := toolbox.StatisticsMap.GetMap()
 	this.Data["Stat"] = statistic["Data"]
 	this.TplName = "statistic.tpl"
